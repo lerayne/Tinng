2c37b55c576296f04f67d2ad548d6f62c2fc727d
Lerayne <lerayne@gmail.com>
2011-11-09 17:39:19 +0200

    some php refaq

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <?php
 
+// подключение универсального инициализатора (база в нем же)
 $rootdir = './';
 require_once 'php/initial.php';
 
-list($path, $getdata) = explode('?', $GLOBALS['_ENV']['REQUEST_URI']);
+// создание переменных окружения
+list($e->path, $e->getdata) = explode('?', $GLOBALS['_ENV']['REQUEST_URI']);
+$e->useragent = $_SERVER['HTTP_USER_AGENT'];
 
+// установка локали
 header ('Content-type:text/html;charset=utf-8;');
 
+// начало работы с интерфейсом
 $columns = array(/*'menu',*/ 'topics', 'posts');
 
-$display_mode = 'desktop'; // режим просмотра в зависимости от устройства
-if (   strpos($_SERVER['HTTP_USER_AGENT'], 'Android') 
-	|| strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile Safari')
-	|| $_GET['mode'] == 'iphone'
-) $display_mode = 'phone';
+// установка режима работы в зависимости от устройства
+$display_mode = 'desktop';
+
+if (strpos($e->useragent, 'Android') || strpos($e->useragent, 'Mobile Safari') || $_GET['mode'] == 'iphone')
+	$display_mode = 'phone';
 
 $device_path = 'displays/'.$display_mode.'/'.$display_mode.'_';
 
+//работа с сессией
 session_start();
 
+// подключение шаблона
 require_once $device_path.'template.php';
 ?>
