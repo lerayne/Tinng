a300ead831a326ecd258ca62a2e4e5b1008036be
Michael Yegorov <lerayne@gmail.com>
2014-01-13 14:17:24 +0200

    пока разрешим капсы в тегах

--- apps/web.main/sources/js/classes/controls/SearchBox.js
+++ apps/web.main/sources/js/classes/controls/SearchBox.js
@@ -320,7 +320,7 @@ tinng.protos.ui.SearchBox.prototype = {
 	tagManualInput:function(tagName){
 
 		if (tagName.replace(this.prefix[this.conf.tagType], '').length >= 3) {
-			this.addTagToSelection(tagName.toLowerCase());
+			this.addTagToSelection(tagName);
 		} else {
 			alert('tag too short!');
 		}
